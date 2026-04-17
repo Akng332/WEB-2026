5371beccd3443fdb40680dafa6b027ff71c75252
Akng332 <deryagin03@mail.ru>
2026-04-18 00:39:47 +0300

    ЛР1 Исправление2

--- ЛР1 Дерягин исправление/app.py
+++ ЛР1 Дерягин исправление/app.py
@@ -73,3 +73,6 @@ def about():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html', title='Страница не найдена'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
